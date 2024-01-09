447638e6892dd0f6ac878cc49c640855eeccb007
Kauã Novaes <kauanovais84@gmail.com>
2024-01-09 16:10:52 -0300

    Creating project and git repository

---
+++ src/db/DbException.java
@@ -0,0 +1,8 @@
+package db;
+
+public class DbException extends RuntimeException{
+    private static final long serialVerionUID = 1L;
+    public DbException(String msg) {
+        super(msg);
+    }
+}
